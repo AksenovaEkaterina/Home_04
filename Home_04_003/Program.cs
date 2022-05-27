28c44b5388fee355ab33a9ee1fdb9664078d6218
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-05-28 00:09:27 +0300

    Home_04_003

---
+++ Home_04_003/Program.cs
@@ -0,0 +1,12 @@
+﻿void NewArray(int n)
+{
+    n = 8;
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write($"{array[i]},");
+    }
+}   
+NewArray(8);
+
